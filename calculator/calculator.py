177a2e304e3a15545a045ba762b5e82d1f64d65a
n1kamgeladze <gg288196@gmail.com>
2024-05-08 20:28:32 +0400

    first commit

---
+++ calculator/calculator.py
@@ -0,0 +1,50 @@
+# დავითის კალკულატორი <3 
+
+a = int(input("enter the first number: "))
+
+choice = input("enter your choice: ")
+
+b = int(input("enter the second number: "))
+
+
+if choice == '+':
+    print(a + b)
+elif choice == "-" :
+    print(a - b)
+elif choice == "*" :
+    print(a * b)
+elif choice == '/':
+    print(a / b)
+else: 
+    print("error")
+
+    #მადლობა დავით ნარიმანიძეს დახმარებისთვის <3 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sorry/sorry.py
@@ -0,0 +1,4 @@
+#sorry Ninoooooooooooooo <3 <3 <3 
+
+for i in range(5001):
+    print("sorry")
